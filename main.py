603667cd5c4e651e7991e653b7629b5060284035
weiqilang <51223813+weiqilang@users.noreply.github.com>
2019-06-22 16:55:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import
+import ujson
+import time
+import MQTT
+import machine
+
+print('***************adding from pythonA')
+print('**************this is a demo ******************')
+print('**************master adding ******************')
+print('**************adding now*******************')
+print('**************adding from master branch')
+print('**************adding from pythonD branch*************')
+print('**************adding from Li*************')
+print('**************adding from YONG*************')
+print('**************adding from Bin*************')
